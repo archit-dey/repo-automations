fe1f2d1a7c47a113a65d641f75b6b064b421bffd
Archit Dey <archit@nenu.co>
2026-01-13 16:07:33 -0800

    Add a new demo feature

--- demo-app/main.go
+++ demo-app/main.go
@@ -187,3 +187,19 @@ func Contains(slice []string, item string) bool {
 	}
 	return false
 }
+
+// Counter returns a closure that increments and returns a counter
+func Counter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
+// Multiplier returns a closure that multiplies by a given factor
+func Multiplier(factor int) func(int) int {
+	return func(x int) int {
+		return x * factor
+	}
+}
